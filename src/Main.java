65fe14f8c2e33deeca5920baffad5f89f1f428f8
kaplanst <kaplanst@mail.ru>
2023-05-10 18:02:18 -0400

    Start  page

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Encrypt(E) or Decrypt (D)");
+        String acting = scan.nextLine();
 
+        if (acting.equals("E")) System.out.println(acting);
 
+        //for int
 
+//        int num= scan.nextInt();
+//
+//        System.out.println(num);
     }
 }
